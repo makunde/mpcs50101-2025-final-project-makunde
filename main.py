d74877913703df1571d75ad9a07ac05f9b0ff2ea
George Martin <georgeg10live@gmail.com>
2025-12-11 00:08:10 -0600

    add docstring for main class

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Fancy task managers with slick user interfaces are slow and cumbersome.
+Now that you have the power of the command line at your disposal, you
+would like be able to access all your tasks without having to burden
+yourself with tedious pointing and clicking. For your final project,
+you will design an object oriented task manager application that will
+allow you to enter tasks, save them to a file, and retrieve them...all
+without moving your hands from the keyboard.
+"""
